5178ed02b4ee34e1b7a1d9b032bfded5fee6f141
Connor Sparks <connorsparks@Connors-iMac.local>
2019-01-01 15:41:57 -0700

    First commit; contains controller with arduino test

---
+++ Control_Test/Control_Test.ino
@@ -0,0 +1,12 @@
+const int pwmIn = 53;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pwmIn,INPUT); 
+}
+
+void loop() {
+  int pwmDisplay = pulseIn(pwmIn, HIGH, 25000);
+  Serial.println(pwmDisplay);
+  delay(5);
+}
