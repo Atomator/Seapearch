2f793b8126d7f4bb7d4c1f594991f6c4b41f82d2
Connor Sparks <30665220+Atomator@users.noreply.github.com>
2019-01-06 17:07:38 -0700

    added second input pin

--- Control_Motor_Test/Control_Motor_Test.ino
+++ Control_Motor_Test/Control_Motor_Test.ino
@@ -6,7 +6,8 @@ const int In1 = 9;
 const int In2 = 8;
 const int In3 = 7;
 const int In4 = 6;
-const int pwmIn = 11;
+const int pwmForward = 11;
+const int pwmTurn = 12;
 
 void setup()
 {
@@ -18,21 +19,22 @@ void setup()
   pinMode(In3, OUTPUT);
   pinMode(In4, OUTPUT);
   Serial.begin(9600);
-  pinMode(pwmIn,INPUT); 
+  pinMode(pwmForward,INPUT); 
+  pinMode(pwmTurn, INPUT);
 }
 
-void goStraight(int pwmSpeed)   //run both motors in the same direction
+void moveMotor(int motor1Speed, int motor2Speed)   //run both motors in the same direction
 {
   // turn on motor A
   digitalWrite(In1, HIGH);
   digitalWrite(In2, LOW);
   // set speed to 150 out 255
-  analogWrite(EnA, pwmSpeed);
+  analogWrite(EnA, motor2Speed);
   // turn on motor B
   digitalWrite(In3, HIGH);
   digitalWrite(In4, LOW);
   // set speed to 150 out 255
-  analogWrite(EnB, pwmSpeed);
+  analogWrite(EnB, motor1Speed);
   delay(5);
 }
 
@@ -47,11 +49,11 @@ void stopMotor()
 void loop()
 {
   
-  int pwmDisplay = pulseIn(pwmIn, HIGH, 25000);
-  int pwmMap = map(pwmDisplay, 890, 2087, 0, 255);
+  int pwmDisplay = pulseIn(pwmForward, HIGH, 25000);
+  int pwmMap = map(pwmDisplay, 1488, 1886, 0, 255);
   Serial.println(pwmMap);
  
-  goStraight(pwmMap);
+  moveMotor(pwmMap, pwmMap);
 
   delay(5);
 }
