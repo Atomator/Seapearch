3581da220018566f8f7c59e7e986a6743871b6f0
Connor Sparks <30665220+Atomator@users.noreply.github.com>
2019-01-07 12:38:30 -0700

    Added basic turning code

--- Control_Motor_Test/Control_Motor_Test.ino
+++ Control_Motor_Test/Control_Motor_Test.ino
@@ -6,8 +6,8 @@ const int In1 = 9;
 const int In2 = 8;
 const int In3 = 7;
 const int In4 = 6;
-const int pwmForward = 11;
-const int pwmTurn = 12;
+const int pulseForward = 11;
+const int pulseTurn = 12;
 
 void setup()
 {
@@ -19,8 +19,8 @@ void setup()
   pinMode(In3, OUTPUT);
   pinMode(In4, OUTPUT);
   Serial.begin(9600);
-  pinMode(pwmForward,INPUT); 
-  pinMode(pwmTurn, INPUT);
+  pinMode(pulseForward,INPUT); 
+  pinMode(pulseTurn, INPUT);
 }
 
 void moveMotor(int motorRight, int motorLeft)   //run both motors in the same direction
@@ -50,15 +50,43 @@ void stopMotor()
 void loop()
 {
 
-  // Puts pulse from pin 12 (pwmForward) into a variable
-  int pwmDisplay = pulseIn(pwmForward, HIGH, 25000);
-  // Maps values from 0 to 255 for use as pwm
-  int pwmMap = map(pwmDisplay, 1488, 1886, 0, 255);
+  // Puts pulse from pin 11 (pulseForward) into a variable
+  int pwmForward = pulseIn(pulseForward, HIGH, 25000);
+  // Maps elev stick values from 0 to 255 for use as pwm
+  int mapForward = map(pwmForward, 1488, 1886, 0, 255);
+  
+  // Puts pulse from pin 12 (pulseForward) into a variable
+  int pwmTurn = pulseIn(pulseTurn, HIGH, 25000);
+  
   // Prints to console for debugging
-  Serial.println(pwmMap);
+  // Serial.println(mapForward);
 
-  // Sets each motor value to pwmMap
-  moveMotor(pwmMap, pwmMap);
+  // 1484 is the middle value, higher than that means the stick is going left, and lower than that means the stick is going right
 
+  // Turns the sub left by slowing down the left motor
+  if (pulseTurn > 1490){
+    int mapTurn = map(pulseTurn, 1490, 1873, mapForward, 0);
+    // moveMotor(mapForward, mapTurn);
+    Serial.print("motorRight: ");
+    Serial.println(mapForward);
+    Serial.print("motorLeft: ");
+    Serial.println(mapTurn);
+  }
+
+  // Turns the sub right by slowing down the right motor
+  if (pulseTurn < 1470){
+    int mapTurn = map(pulseTurn, 1470, 1077, mapForward, 0);
+    // moveMotor(mapTurn, mapForward);
+    Serial.print("motorRight: ");
+    Serial.println(mapTurn);
+    Serial.print("motorLeft: ");
+    Serial.println(mapForward);
+  }
+
+  else {
+    // Sets each motor value to pwmMap
+    // moveMotor(mapForward, mapForward);
+  }
+  
   delay(5);
 }
